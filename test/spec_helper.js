f90866ae9dec7f05aee56dc8fa5fda3e7b387dd1
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-02-21 00:32:46 -0800

    user underscore's deepequal

--- test/spec_helper.js
+++ test/spec_helper.js
@@ -2,25 +2,13 @@ var chai = require('chai');
 var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 
+_ = require('underscore');
+
 chai.Assertion.addMethod('write', function(){
-  var arraysEqual = function(a, b) {
-    if (a === b) return true;
-    if (a === null || b === null) return false;
-    if (a.length != b.length) return false;
-
-    // If you don't care about the order of the elements inside
-    // the array, you should sort both arrays here.
-
-    for (var i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
-    }
-    return true;
-  };
-
   var obj = this._obj;
   var args = Array.prototype.slice.call(arguments, 0);
   this.assert(
-    arraysEqual(obj.write.getCall(0).args[0].args, args),
+    _.isEqual(obj.write.getCall(obj.write.callCount-1).args[0].args, args),
     "expected to write #{exp}, but wrote #{act}",
     "expected not to write #{act}",
     args,

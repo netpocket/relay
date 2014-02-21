0f54d4020aa15d9d769de1b515ab8fc6714719ec
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-02-20 18:01:11 -0800

    move worker logic into app.js

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
-try {
-  var memwatch = require('memwatch');
-  memwatch.on('leak', function(info) {
-    console.log("leak", info);
-  });
-  console.log("Watching for memory leaks");
-} catch (e) {
-  console.log("Not watching for memory leaks -- npm install memwatch to do so");
-}
-
 var config = {
   port: 1337
 };
 
 // Move this to Redis
-var connectedDevices = {},
-    connectedBrowsers = {};
+var connections = {
+  devices: {},
+  browsers: {},
+};
 
 var cluster = require('cluster');
 if (cluster.isMaster) {
@@ -30,82 +22,19 @@ if (cluster.isMaster) {
 } else {
   // the worker
 
-  var Primus = require('primus'),
-      http = require('http'),
-      domain = require('domain'),
-      server = http.createServer(),
-      primusSpec = { transformer: 'sockjs', parser: 'json' },
-      primus = new Primus(server, primusSpec);
-
-  primus.on('connection', function connection(spark) {
-    "use strict";
-
-    var d = domain.create();
-
-    d.on('error', function(err) {
-      console.log("error", err.stack);
-      try {
-        // make sure we close down within 30 seconds
-        var killtimer = setTimeout(function() {
-          process.exit(1);
-        }, 5000);
-        // But don't keep the process open just for that!
-        killtimer.unref();
-
-        // stop taking new requests.
-        server.close();
-
-        // Let the master know we're dead.  This will trigger a
-        // 'disconnect' in the cluster master, and then it will fork
-        // a new worker.
-        cluster.worker.disconnect();
-
-        // try to send an error to the request that triggered the problem
-        spark.write({
-          args: ["relay error", err.stack]
-        });
-      } catch (er2) {
-        // oh well, not much we can do at this point.
-        console.error('Error sending error report to client', er2.stack);
-      }
+  try {
+    var memwatch = require('memwatch');
+    memwatch.on('leak', function(info) {
+      console.log("leak", info);
     });
+    console.log("Watching for memory leaks");
+  } catch (e) {
+    console.log("Not watching for memory leaks -- npm install memwatch to do so");
+  }
 
-    d.add(spark);
+  var App = require('./src/app.js'),
+      app = new App(connections, config);
 
-    var emit = function() {
-      var args = Array.prototype.slice.call(arguments, 0);
-      console.log("emitting", args);
-      spark.write({args:args});
-    };
-
-    d.run(function() {
-      spark.on('data', function (data) {
-        if (spark.reserved(data.args[0])) return;
-        spark.emit.apply(spark, data.args);
-      });
-
-      spark.on('i am a netpocketos device', function(token, info) {
-        connectedDevices[token] = {
-          info: info,
-          spark: spark
-        };
-        console.log(Object.keys(connectedDevices));
-      });
-
-      spark.on('i am a web browser', function(token, info) {
-        connectedBrowsers[token] = {
-          info: info,
-          spark: spark
-        };
-        console.log(Object.keys(connectedBrowsers));
-      });
-
-      emit("please identify");
-
-    });
-  });
-
-  server.listen(config.port);
-  console.log("Listening on "+config.port);
+  app.listen();
 }
 
--- src/app.js
+++ src/app.js
@@ -1,5 +1,83 @@
-module.exports = (function(config) {
+module.exports = (function(connections, config) {
   "use strict";
 
+  var Primus = require('primus'),
+      http = require('http'),
+      domain = require('domain'),
+      server = http.createServer(),
+      primusSpec = { transformer: 'sockjs', parser: 'json' },
+      primus = new Primus(server, primusSpec);
+
+  primus.on('connection', function connection(spark) {
+    var d = domain.create();
+
+    d.on('error', function(err) {
+      console.log("error", err.stack);
+      try {
+        // make sure we close down within 30 seconds
+        var killtimer = setTimeout(function() {
+          process.exit(1);
+        }, 5000);
+        // But don't keep the process open just for that!
+        killtimer.unref();
+
+        // stop taking new requests.
+        server.close();
+
+        // Let the master know we're dead.  This will trigger a
+        // 'disconnect' in the cluster master, and then it will fork
+        // a new worker.
+        cluster.worker.disconnect();
+
+        // try to send an error to the request that triggered the problem
+        spark.write({
+          args: ["relay error", err.stack]
+        });
+      } catch (er2) {
+        // oh well, not much we can do at this point.
+        console.error('Error sending error report to client', er2.stack);
+      }
+    });
+
+    d.add(spark);
+
+    var emit = function() {
+      var args = Array.prototype.slice.call(arguments, 0);
+      console.log("emitting", args);
+      spark.write({args:args});
+    };
+
+    d.run(function() {
+      spark.on('data', function (data) {
+        if (spark.reserved(data.args[0])) return;
+        spark.emit.apply(spark, data.args);
+      });
+
+      spark.on('i am a netpocketos device', function(token, info) {
+        connections.devices[token] = {
+          info: info,
+          spark: spark
+        };
+        console.log(Object.keys(connections.devices));
+      });
+
+      spark.on('i am a web browser', function(token, info) {
+        connections.browsers[token] = {
+          info: info,
+          spark: spark
+        };
+        console.log(Object.keys(connections.browsers));
+      });
+
+      emit("please identify");
+
+    });
+  });
+
+
+  this.listen = function() {
+    server.listen(config.port);
+    console.log("Listening on "+config.port);
+  }
 });
 
